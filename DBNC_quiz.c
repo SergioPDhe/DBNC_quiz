3c497a27917187f6bf94f4ae54ba994d71359f2e
Sergio Ponde Dhelomme <sergio.p.dhe@gmail.com>
2023-01-25 15:11:16 -0500

    Final code w/ comments

--- DBNC_quiz.c
+++ DBNC_quiz.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+// code written by Sergio Dhelomme
+
+//  NOTE: it was unclear if the desired output of the function was a printed list of dimensions or an array.
+//          So i wrote a function that saves all the squares as an array of ints (each int represents the edge length of its square)
+//          as well as a function that prints the full results from the array.
 
 int CutSquare(int *paper);
 int CutPaperIntoSquares(int *paper, int *output);
@@ -9,7 +14,7 @@ void PrintResults(int *output, int outputSize);
 
 void main()
 {
-    int x, y;
+    int x, y;       // side lengths of original paper
 
     printf("Width of paper:");
     scanf("%d", &x);
@@ -17,8 +22,9 @@ void main()
     printf("Length of paper:"); // Get paper dimensions
     scanf("%d", &y);
 
-    int paper[2] = {x,y}; // paper
-    int output[100] = {0}, outputSize;
+    int paper[2] = {x,y};   // paper
+    int output[100] = {0};  // output  
+    int outputSize;         // size of output (number of squares)
 
     outputSize = CutPaperIntoSquares(paper, output);
 
@@ -27,7 +33,7 @@ void main()
 }
 
 
-int CutPaperIntoSquares(int *paper, int *output)
+int CutPaperIntoSquares(int *paper, int *output) // cuts original paper into each square; returns how many squares were cut
 {
     int count = 0;
     
@@ -42,7 +48,7 @@ int CutPaperIntoSquares(int *paper, int *output)
 
 int CutSquare(int *paper) // Cuts the largest possible square out of the paper. Returns the edge length of that square.
 {
-    if(paper[0] > paper[1]) 
+    if(paper[0] > paper[1]) // subtract the shortest length from the largest
     {
         paper[0] -= paper[1];
         return paper[1];
@@ -54,7 +60,7 @@ int CutSquare(int *paper) // Cuts the largest possible square out of the paper. 
     }
 }
 
-void PrintResults(int *output, int outputSize)
+void PrintResults(int *output, int outputSize) // prints the full list of squares from the array of ints
 {
     for(int i=0; i < outputSize; i++)
     {
